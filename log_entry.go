fa2fa1d599bd83aca06ecd72cda158c6b21b82d7
James <jmsadair@gmail.com>
2023-01-04 14:19:32 -0500

    Added get functions and encode/decode functions

--- log_entry.go
+++ log_entry.go
@@ -1,37 +1,78 @@
-package goraft
+package raft
 
-type Command interface{}
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+
+	pb "github.com/jmsadair/raft/protobuf"
+	"google.golang.org/protobuf/proto"
+)
 
 type LogEntry struct {
-	term    uint64
-	index   uint64
-	command Command
+	entry *pb.LogEntry
 }
 
-func NewLogEntry(term uint64, index uint64, command Command) *LogEntry {
-	return &LogEntry{term: term, index: index, command: command}
+func NewLogEntry(term uint64, index uint64, data []byte) *LogEntry {
+	return &LogEntry{entry: &pb.LogEntry{Term: term, Index: index, Data: data}}
 }
 
-func (entry *LogEntry) Term() uint64 {
-	return entry.term
+func (e *LogEntry) Term() uint64 {
+	return e.entry.GetTerm()
 }
 
-func (entry *LogEntry) SetTerm(term uint64) {
-	entry.term = term
+func (e *LogEntry) Index() uint64 {
+	return e.entry.GetIndex()
 }
 
-func (entry *LogEntry) Index() uint64 {
-	return entry.index
+func (e *LogEntry) Data() []byte {
+	return e.entry.GetData()
 }
 
-func (entry *LogEntry) SetIndex(index uint64) {
-	entry.index = index
+func (e *LogEntry) Encode(w io.Writer) (int, error) {
+	var n int
+	var encoded []byte
+	var err error
+
+	if encoded, err = proto.Marshal(e.entry); err != nil {
+		return 0, fmt.Errorf("error encoding log entry: %s", err.Error())
+	}
+
+	// Write length of encoded data that is to follow.
+	buf := make([]byte, 4)
+	binary.LittleEndian.PutUint32(buf, uint32(len(encoded)))
+	if _, err = w.Write(buf); err != nil {
+		return -1, fmt.Errorf("error encoding log entry %s", err.Error())
+	}
+
+	// Write encoded data
+	if n, err = w.Write(encoded); err != nil {
+		return -1, fmt.Errorf("error encoding log entry %s", err.Error())
+	}
+
+	return n, nil
 }
 
-func (entry *LogEntry) Command() Command {
-	return entry.command
-}
+func (e *LogEntry) Decode(r io.Reader) (int, error) {
+	var n int
+	var err error
 
-func (entry *LogEntry) SetCommand(command Command) {
-	entry.command = command
+	// Read the length of encoded data that will follow.
+	buf := make([]byte, 4)
+	if _, err = r.Read(buf); err != nil {
+		return -1, fmt.Errorf("error decoding log entry: %s", err.Error())
+	}
+	length := binary.LittleEndian.Uint32(buf)
+
+	// Read the encoded data.
+	encoded := make([]byte, length)
+	if n, err = r.Read(encoded); err != nil {
+		return -1, fmt.Errorf("error decoding log entry: %s", err.Error())
+	}
+
+	if err = proto.Unmarshal(encoded, e.entry); err != nil {
+		return -1, fmt.Errorf("error decoding log entry: %s", err.Error())
+	}
+
+	return n, err
 }

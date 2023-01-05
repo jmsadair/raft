d6c6256278542559db8815f45a3aff67603c3221
James <jmsadair@gmail.com>
2023-01-05 12:12:31 -0500

    Added additional log functions

--- log.go
+++ log.go
@@ -1,10 +1,15 @@
 package raft
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
 )
+
+// TODO: Reconsider AppendEntries parameters? Need to check prevLogIndex and prevLogTerm provided by leader.
+// TODO: Need to make concurrent safe. Add mutexes.
 
 type Log struct {
 	path        string
@@ -18,14 +23,75 @@ func NewLog(path string) *Log {
 	return &Log{path: filepath.Join(path, "log"), entries: make([]*LogEntry, 0)}
 }
 
+func (l *Log) Path() string {
+	return l.path
+}
+
+func (l *Log) File() *os.File {
+	return l.file
+}
+
+func (l *Log) Size() int {
+	return len(l.entries)
+}
+
+func (l *Log) CommitIndex() uint64 {
+	return l.commitIndex
+}
+
+func (l *Log) LastApplied() uint64 {
+	return l.lastApplied
+}
+
 func (l *Log) Open() error {
-	file, err := os.OpenFile(l.path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile(l.path, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
 	if err != nil {
 		log.Fatalf("failed to open log file: %s", err.Error())
 	}
 	l.file = file
 
-	// TODO: read persisted data into memory.
+	for {
+		entry := NewLogEntry(0, 0, nil)
+		if _, err := entry.Decode(file); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		l.entries = append(l.entries, entry)
+	}
 
 	return nil
+}
+
+func (l *Log) Close() {
+	l.file.Close()
+	l.entries = make([]*LogEntry, 0)
+}
+
+func (l *Log) AppendEntries(entries []*LogEntry) error {
+	panic(fmt.Errorf("AppendEntries: not implemented"))
+}
+
+func (l *Log) AppendEntry(entry *LogEntry) error {
+	// Must persist entry before adding to in-memory log.
+	if _, err := entry.Encode(l.file); err != nil {
+		return err
+	}
+	l.entries = append(l.entries, entry)
+	return nil
+}
+
+func (l *Log) LastIndex() uint64 {
+	if len(l.entries) == 0 {
+		return 0
+	}
+	return l.entries[len(l.entries)-1].Index()
+}
+
+func (l *Log) LastTerm() uint64 {
+	if len(l.entries) == 0 {
+		return 0
+	}
+	return l.entries[len(l.entries)-1].Term()
 }

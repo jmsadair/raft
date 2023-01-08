153392a697abab0dec2d6c37b6649aeb78426835
James <jmsadair@gmail.com>
2023-01-08 12:06:39 -0500

    Refactored test utils

--- testutil.go
+++ testutil.go
@@ -6,18 +6,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func NewTestLog(t *testing.T) *Log {
+func NewTestLog(t *testing.T) *PersistentLog {
 	path := t.TempDir()
-	log := newLog(path)
-	t.Cleanup(func() { log.close() })
-	log.open()
+	log := NewPersistentLog(path)
+	t.Cleanup(func() { log.Close() })
+	log.Open()
 	return log
 }
 
 func validateLogEntry(t *testing.T, entry *LogEntry, expectedIndex uint64, expectedTerm uint64, expectedData []byte) {
-	assert.Equal(t, expectedIndex, entry.index(), "entry has incorrect index")
-	assert.Equal(t, expectedTerm, entry.term(), "entry has incorrect term")
-	assert.Equal(t, expectedData, entry.data(), "entry has incorrect data")
+	assert.Equal(t, expectedIndex, entry.Index(), "entry has incorrect index")
+	assert.Equal(t, expectedTerm, entry.Term(), "entry has incorrect term")
+	assert.Equal(t, expectedData, entry.Data(), "entry has incorrect data")
 }
 
 func validateLogSize(t *testing.T, actualSize int, expectedSize int) {

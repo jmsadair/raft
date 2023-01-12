30d0a57743846ccd40e827340f56aeb2bda6cba4
James <jmsadair@gmail.com>
2023-01-12 12:25:43 -0500

    Implemented RandomTimout function

--- internal/util/util.go
+++ internal/util/util.go
@@ -1,10 +1,21 @@
 package util
 
-import "golang.org/x/exp/constraints"
+import (
+	"math/rand"
+	"time"
+
+	"golang.org/x/exp/constraints"
+)
 
 func Min[T constraints.Ordered](a, b T) T {
 	if a < b {
 		return a
 	}
 	return b
+}
+
+func RandomTimeout(min time.Duration, max time.Duration) <-chan time.Time {
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Int63n(max.Milliseconds()-min.Milliseconds()) + min.Milliseconds()
+	return time.After(time.Duration(n) * time.Millisecond)
 }

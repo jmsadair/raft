182090ad254c42df7b0a402a67f5607cf1ece3c8
James <jmsadair@gmail.com>
2023-01-12 12:26:49 -0500

    Implemented initial follower and candidate loops

--- raft.go
+++ raft.go
@@ -1,6 +1,9 @@
 package raft
 
 import (
+	"sync"
+	"time"
+
 	logger "github.com/jmsadair/raft/internal/logger"
 	pb "github.com/jmsadair/raft/internal/protobuf"
 	"github.com/jmsadair/raft/internal/util"
@@ -27,18 +30,10 @@ func (s State) String() string {
 	}
 }
 
-type Config struct {
-	// The minimum election timeout, in milliseconds.
-	minElectionTimeout uint64
-
-	// The maximum election timeout, in milliseconds.
-	maxElectionTimeout uint64
-
-	// Time between heartbeats, in milliseconds.
-	heartbeat uint64
-}
-
 type Raft struct {
+	// Timing and logger configuration.
+	config *Config
+
 	// The ID of this raft server.
 	id string
 
@@ -64,6 +59,11 @@ type Raft struct {
 	nextIndex  map[string]uint64
 	matchIndex map[string]uint64
 
+	// Time at which server was last contacted.
+	lastContact time.Time
+
+	mu sync.Mutex
+
 	logger logger.Logger
 }
 
@@ -72,25 +72,23 @@ func (r *Raft) Submit(command any) error {
 }
 
 func (r *Raft) appendEntries(request *pb.AppendEntriesRequest) *pb.AppendEntriesResponse {
-	response := &pb.AppendEntriesResponse{Term: r.currentTerm, Success: false}
+	response := &pb.AppendEntriesResponse{Term: r.getCurrentTerm(), Success: false}
 
-	if request.GetTerm() < r.currentTerm {
-		r.logger.Debugf("server %s rejected request to append entries: %d < %d (term < currentTerm)",
-			r.id, request.GetTerm(), r.currentTerm)
+	r.setLastContact(time.Now())
+
+	if request.GetTerm() < r.getCurrentTerm() {
 		return response
 	}
 
 	if request.GetTerm() > r.currentTerm {
-		r.currentTerm = request.GetTerm()
-		response.Term = r.currentTerm
+		r.setCurrentTerm(request.GetTerm())
+		response.Term = r.getCurrentTerm()
 		defer r.becomeFollower()
 	}
 
 	r.leader = request.GetLeaderId()
 
 	if !r.log.Contains(request.GetPrevLogIndex()) || r.log.GetEntry(request.GetPrevLogIndex()).Term() != request.GetTerm() {
-		r.logger.Debugf("server %s rejected request to append entries: prevLogIndex %d does not have term that matches %d",
-			r.id, request.GetPrevLogIndex(), request.GetPrevLogTerm())
 		return response
 	}
 
@@ -110,22 +108,22 @@ func (r *Raft) appendEntries(request *pb.AppendEntriesRequest) *pb.AppendEntries
 }
 
 func (r *Raft) requestVote(request *pb.RequestVoteRequest) *pb.RequestVoteResponse {
-	response := &pb.RequestVoteResponse{Term: r.currentTerm, VoteGranted: false}
+	response := &pb.RequestVoteResponse{Term: r.getCurrentTerm(), VoteGranted: false}
 
-	if request.GetTerm() < r.currentTerm {
+	if request.GetTerm() < r.getCurrentTerm() {
 		return response
 	}
 
-	if request.GetTerm() > r.currentTerm {
-		r.currentTerm = request.GetTerm()
-		response.Term = r.currentTerm
+	if request.GetTerm() > r.getCurrentTerm() {
+		r.setCurrentTerm(request.GetTerm())
+		response.Term = r.getCurrentTerm()
 		defer r.becomeFollower()
 	}
 
 	lastTerm := r.log.LastTerm()
 	lastIndex := r.log.LastIndex()
 
-	if r.votedFor != "" && r.votedFor != request.GetCandidateId() {
+	if r.getVotedFor() != "" && r.getVotedFor() != request.GetCandidateId() {
 		return response
 	}
 
@@ -140,38 +138,152 @@ func (r *Raft) requestVote(request *pb.RequestVoteRequest) *pb.RequestVoteRespon
 	return response
 }
 
-func (r *Raft) sendAppendEntries() {
-	panic("SendAppendEntries: not implemented")
-}
-
 func (r *Raft) leaderLoop() {
 	panic("leaderLoop: not implemented")
 }
 
 func (r *Raft) followerLoop() {
-	panic("followerLoop: not implemented")
+	electionTimeout := util.RandomTimeout(r.config.ElectionTimeout, r.config.ElectionTimeout*2)
+
+	for {
+		select {
+		case <-electionTimeout:
+			electionTimeout = util.RandomTimeout(r.config.ElectionTimeout, r.config.ElectionTimeout*2)
+			if time.Since(r.getLastContact()) > r.config.ElectionTimeout {
+				r.becomeCandidate()
+				return
+			}
+		default:
+			if r.getState() != Follower {
+				return
+			}
+		}
+	}
 }
 
 func (r *Raft) candidateLoop() {
-	panic("candidateLoop: not implemented")
+	votesReceived := 1
+	r.setCurrentTerm(r.getCurrentTerm() + 1)
+
+	request := pb.RequestVoteRequest{
+		CandidateId:  r.id,
+		Term:         r.getCurrentTerm(),
+		LastLogIndex: r.log.LastIndex(),
+		LastLogTerm:  r.log.LastTerm()}
+
+	responses := make(chan *pb.RequestVoteResponse)
+	electionTimeout := util.RandomTimeout(r.config.ElectionTimeout, r.config.ElectionTimeout*2)
+
+	collectPeerVote := func(peer *Peer, responses chan<- *pb.RequestVoteResponse) {
+		response, _ := peer.RequestVote(&request)
+		responses <- response
+	}
+
+	for _, peer := range r.peers {
+		go collectPeerVote(peer, responses)
+	}
+
+	for {
+		select {
+		case response := <-responses:
+			if response.GetTerm() > r.getCurrentTerm() {
+				r.setCurrentTerm(response.GetTerm())
+				r.becomeFollower()
+				return
+			}
+			if response.VoteGranted {
+				votesReceived++
+			}
+			if votesReceived >= r.quorum() {
+				r.becomeLeader()
+				return
+			}
+		case <-electionTimeout:
+			return
+		default:
+			if r.getState() != Candidate {
+				return
+			}
+		}
+	}
 }
 
 func (r *Raft) mainLoop() {
-	panic("mainLoop: not implemented")
+	for {
+		switch r.getState() {
+		case Candidate:
+			r.candidateLoop()
+		case Leader:
+			r.leaderLoop()
+		case Follower:
+			r.followerLoop()
+		}
+	}
+}
+
+func (r *Raft) becomeCandidate() {
+	r.setState(Candidate)
 }
 
 func (r *Raft) becomeLeader() {
-	r.state = Leader
+	r.setState(Leader)
 	r.nextIndex = make(map[string]uint64)
 	r.matchIndex = make(map[string]uint64)
 }
 
 func (r *Raft) becomeFollower() {
-	r.votedFor = ""
-	r.state = Follower
+	r.setVotedFor("")
+	r.setState(Follower)
 }
 
-func (r *Raft) becomeCandidate() {
-	r.currentTerm += 1
-	r.state = Candidate
+func (r *Raft) quorum() int {
+	return len(r.peers)/2 + 1
+}
+
+func (r *Raft) setState(state State) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.state = state
+}
+
+func (r *Raft) getState() State {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	return r.state
+}
+
+func (r *Raft) setCurrentTerm(term uint64) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.currentTerm = term
+}
+
+func (r *Raft) getCurrentTerm() uint64 {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	return r.currentTerm
+}
+
+func (r *Raft) setVotedFor(votedFor string) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.votedFor = votedFor
+}
+
+func (r *Raft) getVotedFor() string {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	return r.votedFor
+}
+
+func (r *Raft) setLastContact(time time.Time) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.lastContact = time
+}
+
+func (r *Raft) getLastContact() time.Time {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	return r.lastContact
 }

6386f1b03286f239c6ec91acb290c77a4357c7f4
James <jmsadair@gmail.com>
2023-01-15 17:45:33 -0500

    Refactored Peer struct

--- peer.go
+++ peer.go
@@ -3,15 +3,19 @@ package raft
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	pb "github.com/jmsadair/raft/internal/protobuf"
 	"google.golang.org/grpc"
 )
 
 type Peer struct {
-	id      string
-	address string
-	client  pb.RaftClient
+	id         string
+	address    string
+	nextIndex  uint64
+	matchIndex uint64
+	client     pb.RaftClient
+	mu         sync.Mutex
 }
 
 func NewPeer(id, address string) *Peer {
@@ -55,4 +59,28 @@ func (p *Peer) Address() string {
 
 func (p *Peer) SetAddress(address string) {
 	p.address = address
+}
+
+func (p *Peer) setNextIndex(index uint64) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.nextIndex = index
+}
+
+func (p *Peer) getNextIndex() uint64 {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	return p.nextIndex
+}
+
+func (p *Peer) setMatchIndex(index uint64) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.matchIndex = index
+}
+
+func (p *Peer) getMatchIndex() uint64 {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	return p.matchIndex
 }

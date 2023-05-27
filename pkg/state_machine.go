4ec7198b3c7621cb3db699354e64fd0e5bde0430
James <jmsadair@gmail.com>
2023-05-27 14:40:16 -0400

    Updated documentation

--- pkg/state_machine.go
+++ pkg/state_machine.go
@@ -1,14 +1,25 @@
 package raft
 
+// StateMachine is an interface representing a replicated state machine.
 type StateMachine interface {
-	// Apply applies a command to the state machine.
+	// Apply applies a command to the state machine and returns the result.
+	// Parameters:
+	//     - command: The command to apply to the state machine.
+	// Returns:
+	//     - interface{}: The result of applying the command to the state machine.
 	Apply(command []byte) interface{}
 
-	// Snapshot returns a snapshot of the current state of the state
-	// machine. The returned bytes must be serialized in a way that
-	// the Restore function can understand.
+	// Snapshot returns a snapshot of the current state of the state machine.
+	// The returned bytes must be serialized in a way that the Restore function can understand.
+	// Returns:
+	//     - []byte: The serialized snapshot of the current state.
+	//     - error: An error if creating the snapshot fails, or nil otherwise.
 	Snapshot() ([]byte, error)
 
 	// Restore recovers the state of the state machine given a snapshot.
+	// Parameters:
+	//     - snapshot: The serialized snapshot representing the state of the state machine.
+	// Returns:
+	//     - error: An error if restoring the state fails, or nil otherwise.
 	Restore(snapshot []byte) error
 }
--- pkg/storage.go
+++ pkg/storage.go
@@ -6,20 +6,29 @@ import (
 	"os"
 )
 
-// Storage defines the interface for a storage object that can persist state.
+// Storage is an interface representing a storage object that can persist state.
 type Storage interface {
-	// Open opens the storage for persisting state. Open must be called
-	// before SetState or GetState are called.
+	// Open opens the storage for persisting state. Open must be called before calling SetState or GetState.
+	// Returns:
+	//     - error: An error if opening the storage fails, or nil otherwise.
 	Open() error
 
-	// Close closes the storage. After Close is called, SetState and GetState
-	// should not be called.
+	// Close closes the storage. After Close is called, SetState and GetState should not be called.
+	// Returns:
+	//     - error: An error if closing the storage fails, or nil otherwise.
 	Close() error
 
-	// SetState persists the provided state in storage.
+	// SetState persists the provided state in the storage.
+	// Parameters:
+	//     - persistentState: The state to be persisted.
+	// Returns:
+	//     - error: An error if persisting the state fails, or nil otherwise.
 	SetState(persistentState *PersistentState) error
 
-	// GetState recovers the state from storage.
+	// GetState recovers the state from the storage.
+	// Returns:
+	//     - PersistentState: The recovered state.
+	//     - error: An error if recovering the state fails, or nil otherwise.
 	GetState() (PersistentState, error)
 }
 
@@ -45,6 +54,15 @@ type PersistentState struct {
 	votedFor string
 }
 
+// NewPersistentStorage creates a new instance of PersistentStorage with the provided path, storage encoder,
+// and storage decoder.
+// Parameters:
+//   - path: The path where the persistent storage is located.
+//   - storageEncoder: The storage encoder responsible for encoding PersistentState instances into a binary format.
+//   - storageDecoder: The storage decoder responsible for decoding binary data into PersistentState instances.
+//
+// Returns:
+//   - *PersistentStorage: A new instance of PersistentStorage.
 func NewPersistentStorage(path string, storageEncoder StorageEncoder, storageDecoder StorageDecoder) *PersistentStorage {
 	return &PersistentStorage{path: path, storageEncoder: storageEncoder, storageDecoder: storageDecoder}
 }

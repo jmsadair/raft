238bab62d6dc122e8d2271ab8d3894667cb55051
James <jmsadair@gmail.com>
2023-05-29 17:53:11 -0400

    Added test for snapshot encoder and decoder

--- pkg/encoding_test.go
+++ pkg/encoding_test.go
@@ -45,3 +45,23 @@ func TestProtoStorageEncoderDecoder(t *testing.T) {
 	assert.Equal(t, persistentState.term, decodedState.term, "decoded state has incorrect term")
 	assert.Equal(t, persistentState.votedFor, decodedState.votedFor, "decoded state has incorrect votedFor")
 }
+
+func TestProtoSnapshotEncoderDecoder(t *testing.T) {
+	snapshot := Snapshot{LastIncludedIndex: 1, LastIncludedTerm: 1, Data: []byte("test")}
+	buf := new(bytes.Buffer)
+
+	encoder := new(ProtoSnapshotEncoder)
+	if err := encoder.Encode(buf, &snapshot); err != nil {
+		t.Fatalf("failed to encode persistent state : %s", err.Error())
+	}
+
+	decoder := new(ProtoSnapshotDecoder)
+	decodedSnapshot, err := decoder.Decode(buf)
+	if err != nil {
+		t.Fatalf("failed to decode persistent state: %s", err.Error())
+	}
+
+	assert.Equal(t, snapshot.LastIncludedIndex, decodedSnapshot.LastIncludedIndex, "decoded snapshot has incorrect last included term")
+	assert.Equal(t, snapshot.LastIncludedTerm, decodedSnapshot.LastIncludedTerm, "decoded snapshot has incorrect last included index")
+	assert.Equal(t, snapshot.Data, decodedSnapshot.Data, "decoded snapshot has incorrect data")
+}

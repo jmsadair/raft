91366f6c583b320799fc545d8fb3317c4d6071dd
James <jmsadair@gmail.com>
2023-06-03 14:16:16 -0400

    Refactored state machine

--- pkg/state_machine.go
+++ pkg/state_machine.go
@@ -2,15 +2,15 @@ package raft
 
 // StateMachine is an interface representing a replicated state machine.
 type StateMachine interface {
-	// Apply applies a command with the associated index and term
-	// to the state machine and returns the result.
-	Apply(command []byte, index uint64, term uint64) interface{}
+	// Apply applies the given log entry to the state machine.
+	Apply(entry *LogEntry) interface{}
 
 	// Snapshot returns a snapshot of the current state of the state machine.
-	// The returned bytes must be serialized in a way that the Restore function can understand.
-	Snapshot() ([]byte, error)
+	// The bytes contained in the snapshot must be serialized in a way that
+	// the Restore function can understand.
+	Snapshot() (Snapshot, error)
 
 	// Restore recovers the state of the state machine given a snapshot that was produced
 	// by Snapshot.
-	Restore(snapshot []byte) error
+	Restore(snapshot *Snapshot) error
 }

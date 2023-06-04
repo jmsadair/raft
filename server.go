223273e64beed42ba36b2215a9d616e7895e5636
James <jmsadair@gmail.com>
2023-06-04 14:07:58 -0400

    Fixed server documentation

--- server.go
+++ server.go
@@ -10,7 +10,7 @@ import (
 	"google.golang.org/grpc"
 )
 
-// GrpcServer is a wrapper for a Raft instance. It serves requests
+// GrpcServer is a wrapper for a RaftCore instance. It serves requests
 // for Raft using protobuf and gRPC.
 type GrpcServer struct {
 	pb.UnimplementedRaftServer
@@ -21,7 +21,7 @@ type GrpcServer struct {
 	wg              sync.WaitGroup
 }
 
-// NewProtobufServer creates a new instance of ProtobufServer.
+// NewGrpcServer creates a new instance of GrpcServer.
 func NewGrpcServer(
 	id string,
 	peers []*GrpcPeer,
@@ -50,7 +50,7 @@ func NewGrpcServer(
 	return server, nil
 }
 
-// Start starts the ProtobufServer. It listens for incoming connections on the configured
+// Start starts the GrpcServer. It listens for incoming connections on the configured
 // network address and starts the Raft instance. It also starts serving gRPC requests on the
 // listener. The provided channel is used to signal to the server that it should start serving
 // requests.
@@ -76,7 +76,7 @@ func (s *GrpcServer) Start(ready <-chan interface{}) error {
 	return nil
 }
 
-// Stop stops the ProtobufServer.
+// Stop stops the GrpcServer.
 // It gracefully stops the gRPC server, stops the Raft instance, and closes the listener.
 func (s *GrpcServer) Stop() {
 	if s.server == nil {
@@ -90,20 +90,20 @@ func (s *GrpcServer) Stop() {
 	s.server = nil
 }
 
-// Status returns the status of the ProtobufServer.
+// Status returns the status of the GrpcServer.
 // It retrieves the status from the underlying Raft instance.
 func (s *GrpcServer) Status() Status {
 	return s.raft.Status()
 }
 
-// IsStarted checks if the ProtobufServer is started.
+// IsStarted checks if the GrpcServer is started.
 // It returns true if the server is started, false otherwise.
 func (s *GrpcServer) IsStarted() bool {
 	return s.server != nil
 }
 
-// SubmitCommand submits a command to the ProtobufServer for processing.
-// It forwards the command to the underlying Raft instance for handling
+// SubmitCommand submits a command to the GrpcServer for processing.
+// It forwards the command to the underlying RaftCore instance for handling
 // and returns the index and term assigned to the command, as well as
 // an error if submitting the command failed.
 func (s *GrpcServer) SubmitCommand(command Command) (uint64, uint64, error) {
@@ -111,7 +111,7 @@ func (s *GrpcServer) SubmitCommand(command Command) (uint64, uint64, error) {
 }
 
 // AppendEntries handles the AppendEntries gRPC request.
-// It converts the request to the internal representation, invokes the AppendEntries function on the Raft instance,
+// It converts the request to the internal representation, invokes the AppendEntries function on the RaftCore instance,
 // and returns the response.
 func (s *GrpcServer) AppendEntries(ctx context.Context, request *pb.AppendEntriesRequest) (*pb.AppendEntriesResponse, error) {
 	appendEntriesRequest := makeAppendEntriesRequest(request)
@@ -123,7 +123,7 @@ func (s *GrpcServer) AppendEntries(ctx context.Context, request *pb.AppendEntrie
 }
 
 // RequestVote handles the RequestVote gRPC request.
-// It converts the request to the internal representation, invokes the RequestVote function on the Raft instance,
+// It converts the request to the internal representation, invokes the RequestVote function on the RaftCore instance,
 // and returns the response.
 func (s *GrpcServer) RequestVote(ctx context.Context, request *pb.RequestVoteRequest) (*pb.RequestVoteResponse, error) {
 	requestVoteRequest := makeRequestVoteRequest(request)
@@ -135,7 +135,7 @@ func (s *GrpcServer) RequestVote(ctx context.Context, request *pb.RequestVoteReq
 }
 
 // InstallSnapshot handles the InstallSnapshot gRPC request.
-// It converts the request to the internal representation, invokes the InstallSnapshot function on the Raft instance,
+// It converts the request to the internal representation, invokes the InstallSnapshot function on the RaftCore instance,
 // and returns the response.
 func (s *GrpcServer) InstallSnapshot(ctx context.Context, request *pb.InstallSnapshotRequest) (*pb.InstallSnapshotResponse, error) {
 	installSnapshotRequest := makeInstallSnapshotRequest(request)

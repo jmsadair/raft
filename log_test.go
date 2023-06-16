74b4572df2f7ec0b03eaf7684d1b2462aff19909
James <jmsadair@gmail.com>
2023-06-16 11:09:46 -0400

    Added additional test for log

--- log_test.go
+++ log_test.go
@@ -21,6 +21,7 @@ func TestAppendEntries(t *testing.T) {
 
 	defer log.Close()
 
+	// Add some entries to the log.
 	var entry1, entry2 *LogEntry
 	var err error
 
@@ -38,6 +39,7 @@ func TestAppendEntries(t *testing.T) {
 		t.Fatalf("error appending entries to log: %s", err.Error())
 	}
 
+	// Make sure the added entries are correct.
 	entry1, err = log.GetEntry(entry1Index)
 	if err != nil {
 		t.Fatalf("error getting entry from log: %s", err.Error())
@@ -53,6 +55,7 @@ func TestAppendEntries(t *testing.T) {
 	assert.Equal(t, log.LastTerm(), entry2Term)
 	assert.Equal(t, log.LastIndex(), entry2Index)
 
+	// Close and reopen to the log to check that it was persisted correctly.
 	if err := log.Close(); err != nil {
 		t.Fatalf("error closing log: %s", err.Error())
 	}
@@ -95,6 +98,7 @@ func TestTruncate(t *testing.T) {
 
 	defer log.Close()
 
+	// Add some entries to the log.
 	var err error
 	var entry1, entry2, entry3 *LogEntry
 
@@ -115,10 +119,12 @@ func TestTruncate(t *testing.T) {
 
 	log.AppendEntries([]*LogEntry{entry1, entry2, entry3})
 
+	// Truncate the log down to and including the second entry.
 	if err := log.Truncate(entry2Index); err != nil {
 		t.Fatalf("error truncating log: %s", err.Error())
 	}
 
+	// Make sure the first entry is still present and correct.
 	entry1, err = log.GetEntry(entry1Index)
 	if err != nil {
 		t.Fatalf("error getting entry from log: %s", err.Error())
@@ -128,6 +134,7 @@ func TestTruncate(t *testing.T) {
 	assert.Equal(t, log.LastTerm(), entry1Term)
 	assert.Equal(t, log.LastIndex(), entry1Index)
 
+	// Close and reopen the log to make sure it was persisted correctly.
 	if err := log.Close(); err != nil {
 		t.Fatalf("error closing log: %s", err.Error())
 	}
@@ -162,6 +169,7 @@ func TestCompact(t *testing.T) {
 
 	defer log.Close()
 
+	// Add some entries to the log.
 	var err error
 	var entry1, entry2, entry3, entry4 *LogEntry
 
@@ -182,10 +190,12 @@ func TestCompact(t *testing.T) {
 
 	log.AppendEntries([]*LogEntry{entry1, entry2, entry3})
 
+	// Compact the log up to and including the second index.
 	if err := log.Compact(entry2Index); err != nil {
 		t.Fatalf("error compacting log: %s", err.Error())
 	}
 
+	// Make sure the third entry is still present and correct.
 	entry3, err = log.GetEntry(entry3Index)
 	if err != nil {
 		t.Fatalf("error getting entry from log: %s", err.Error())
@@ -195,6 +205,7 @@ func TestCompact(t *testing.T) {
 	assert.Equal(t, log.LastTerm(), entry3Term)
 	assert.Equal(t, log.LastIndex(), entry3Index)
 
+	// Make sure we can still add and retrieve entries from the log.
 	var entry4Index uint64 = 4
 	var entry4Term uint64 = 2
 	entry4Data := []byte("entry4")
@@ -208,6 +219,7 @@ func TestCompact(t *testing.T) {
 	}
 	validateLogEntry(t, entry4, entry4Index, entry4Term, entry4Data)
 
+	// Close and reopen to the log to make sure the it was correctly persisted.
 	if err := log.Close(); err != nil {
 		t.Fatalf("error closing log: %s", err.Error())
 	}
@@ -231,4 +243,44 @@ func TestCompact(t *testing.T) {
 		t.Fatalf("error getting entry from log: %s", err.Error())
 	}
 	validateLogEntry(t, entry4, entry4Index, entry4Term, entry4Data)
+}
+
+func TestDiscard(t *testing.T) {
+	tmpDir := t.TempDir()
+	path := tmpDir + "/test-log.bin"
+	log := newPersistentLog(path)
+
+	if err := log.Open(); err != nil {
+		t.Fatalf("error opening log: %s", err.Error())
+	}
+
+	if err := log.Replay(); err != nil {
+		t.Fatalf("error replaying log: %s", err.Error())
+	}
+
+	defer log.Close()
+
+	// Add some log entries to the log.
+	var entry1Index uint64 = 1
+	var entry1Term uint64 = 1
+	entry1Data := []byte("entry1")
+	entry1 := NewLogEntry(entry1Index, entry1Term, entry1Data)
+
+	var entry2Index uint64 = 2
+	var entry2Term uint64 = 4
+	entry2Data := []byte("entry2")
+	entry2 := NewLogEntry(entry2Index, entry2Term, entry2Data)
+
+	log.AppendEntries([]*LogEntry{entry1, entry2})
+
+	// Discard the log entries.
+	var discardIndex uint64 = 5
+	var discardTerm uint64 = 5
+	if err := log.DiscardEntries(discardIndex, discardTerm); err != nil {
+		t.Fatalf("error discarding log entries: %s", err.Error())
+	}
+
+	// Make sure the last index and last term are correct.
+	assert.Equal(t, discardIndex, log.LastIndex(), "last index not correct after discarding log entries")
+	assert.Equal(t, discardTerm, log.LastTerm(), "last term not correct after discarding log entries")
 }

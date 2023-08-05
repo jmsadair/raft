1c78d1db65f6ca289a871df2db84464924f82c6a
James <jmsadair@gmail.com>
2023-08-05 13:48:58 -0400

    Added tests for failed AppendEntries request

--- raft_test.go
+++ raft_test.go
@@ -42,12 +42,10 @@ func TestAppendEntriesSuccess(t *testing.T) {
 
 	entries := []*LogEntry{NewLogEntry(1, 1, []byte("test1"))}
 	request := &AppendEntriesRequest{
-		Entries:      entries,
 		LeaderID:     "test-leader",
-		LeaderCommit: 1,
 		Term:         1,
-		PrevLogIndex: 0,
-		PrevLogTerm:  0,
+		LeaderCommit: 1,
+		Entries:      entries,
 	}
 	response := &AppendEntriesResponse{}
 
@@ -77,12 +75,9 @@ func TestAppendEntriesConflictSuccess(t *testing.T) {
 
 	entries := []*LogEntry{NewLogEntry(1, 1, []byte("test1")), NewLogEntry(2, 1, []byte("test2"))}
 	request := &AppendEntriesRequest{
-		Entries:      entries,
-		LeaderID:     "test-leader",
-		LeaderCommit: 0,
-		Term:         2,
-		PrevLogIndex: 0,
-		PrevLogTerm:  0,
+		LeaderID: "test-leader",
+		Term:     2,
+		Entries:  entries,
 	}
 	response := &AppendEntriesResponse{}
 
@@ -120,12 +115,9 @@ func TestAppendEntriesLeaderStepDownSuccess(t *testing.T) {
 	raft.state = Leader
 
 	request := &AppendEntriesRequest{
-		Entries:      []*LogEntry{},
-		LeaderID:     "other-test-leader",
-		LeaderCommit: 0,
-		Term:         3,
-		PrevLogIndex: 0,
-		PrevLogTerm:  0,
+		LeaderID: "other-test-leader",
+		Term:     3,
+		Entries:  []*LogEntry{},
 	}
 	response := &AppendEntriesResponse{}
 
@@ -136,4 +128,72 @@ func TestAppendEntriesLeaderStepDownSuccess(t *testing.T) {
 	require.Equal(t, uint64(3), raft.currentTerm)
 	require.Equal(t, Follower, raft.state)
 	require.Equal(t, "", raft.votedFor)
+}
+
+// TestAppendEntriesOutOfDateTermFailure checks that an AppendEntries request received that has a term less than
+// the term of server that received it is rejected.
+func TestAppendEntriesOutOfDateTermFailure(t *testing.T) {
+	args := makeRaftArgs(t, false, 0)
+
+	raft, err := NewRaft(args.id, args.peers, args.log, args.storage, args.snapshotStorage, args.stateMachine, args.responseCh)
+	require.NoError(t, err)
+
+	raft.state = Follower
+	raft.currentTerm = 2
+
+	request := &AppendEntriesRequest{
+		LeaderID: "test-leader",
+		Term:     1,
+		Entries:  []*LogEntry{},
+	}
+	response := &AppendEntriesResponse{}
+
+	require.NoError(t, raft.AppendEntries(request, response))
+	require.False(t, response.Success)
+	require.Equal(t, uint64(2), response.Term)
+}
+
+// TestAppendEntriesPrevLogIndexFailure checks that an AppendEntries request is rejected when the log does not contain
+// an entry at the previous log index whose term match the previous log term.
+func TestAppendEntriesPrevLogIndexFailure(t *testing.T) {
+	args := makeRaftArgs(t, false, 0)
+
+	raft, err := NewRaft(args.id, args.peers, args.log, args.storage, args.snapshotStorage, args.stateMachine, args.responseCh)
+	require.NoError(t, err)
+
+	raft.state = Follower
+	raft.currentTerm = 1
+	require.NoError(t, raft.log.AppendEntry(NewLogEntry(1, 1, []byte("test"))))
+
+	request := &AppendEntriesRequest{
+		LeaderID:     "test-leader",
+		Term:         1,
+		Entries:      []*LogEntry{},
+		PrevLogTerm:  2,
+		PrevLogIndex: 1,
+	}
+	response := &AppendEntriesResponse{}
+
+	require.NoError(t, raft.AppendEntries(request, response))
+	require.False(t, response.Success)
+	require.Equal(t, uint64(1), response.Term)
+}
+
+// TestAppendEntriesShutdownFailure checks that an error is returned when an AppendEntries request is received
+// by a server in the shutdown state.
+func TestAppendEntriesShutdownFailure(t *testing.T) {
+	args := makeRaftArgs(t, false, 0)
+
+	raft, err := NewRaft(args.id, args.peers, args.log, args.storage, args.snapshotStorage, args.stateMachine, args.responseCh)
+	require.NoError(t, err)
+
+	request := &AppendEntriesRequest{
+		LeaderID: "test-leader",
+		Term:     1,
+		Entries:  []*LogEntry{},
+	}
+	response := &AppendEntriesResponse{}
+
+	require.Error(t, raft.AppendEntries(request, response))
+	require.False(t, response.Success)
 }

146bee675cdcb9aabf22905f5158c5987499f018
James <jmsadair@gmail.com>
2023-12-25 10:56:50 -0500

    Rewrote doc.go

--- doc.go
+++ doc.go
@@ -1,9 +1,6 @@
-/*
-Package raft is an implementation of the raft consensus protocol.
-
-This library provides a simple, easy-to-understand, and reliable implementation of Raft using Go. Raft is a consensus
-protocol designed to manage replicated logs in a distributed system. Its purpose is to ensure fault-tolerant coordination
-and consistency among a group of nodes, making it suitable for building reliable systems. Potential use cases include
-distributed file systems, consistent key-value stores, and service discovery.
-*/ //
+// Package raft provides a simple, easy-to-understand, and reliable implementation of Raft.
+//
+// Raft is a consensus protocol designed to manage replicated logs in a distributed system. Its purpose is to ensure fault-tolerant coordination
+// and consistency among a group of nodes, making it suitable for building reliable systems. Potential use cases include
+// distributed file systems, consistent key-value stores, and service discovery.
 package raft

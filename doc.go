23471c15a89cd340cd8184f939b1ccb3c74ce73a
James Adair <jmsadair@gmail.com>
2023-12-25 15:43:08 +0000

    Committing go fmt changes

--- doc.go
+++ doc.go
@@ -5,5 +5,4 @@ This library provides a simple, easy-to-understand, and reliable implementation 
 protocol designed to manage replicated logs in a distributed system. Its purpose is to ensure fault-tolerant coordination
 and consistency among a group of nodes, making it suitable for building reliable systems. Potential use cases include
 distributed file systems, consistent key-value stores, and service discovery.
-*/
-package raft
+*/package raft

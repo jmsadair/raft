dc7a794244cb3244b8a6ec214f5839c98cd1bf9c
James <jmsadair@gmail.com>
2024-01-19 15:09:37 -0500

    Fixed staticcheck errors

--- internal/util/util.go
+++ internal/util/util.go
@@ -27,13 +27,11 @@ func Max[T constraints.Ordered](a, b T) T {
 
 // RandomTimeout generates a random duration between the provided min and max durations.
 func RandomTimeout(min time.Duration, max time.Duration) time.Duration {
-	rand.Seed(time.Now().UnixNano())
 	n := rand.Int63n(max.Milliseconds()-min.Milliseconds()) + min.Milliseconds()
 	return time.Duration(n)
 }
 
 // RandomInt generates a random integer between the provided min and max integers (inclusive of min, exclusive of max).
 func RandomInt(min int, max int) int {
-	rand.Seed(time.Now().UnixNano())
 	return min + rand.Intn(max-min)
 }

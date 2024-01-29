90aa68d77e27528ce13998fc51484198876bad71
James <jmsadair@gmail.com>
2024-01-29 15:18:37 -0500

    Added tests for transport

---
+++ transport_test.go
@@ -0,0 +1,23 @@
+package raft
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestConfigurationEncoderDecoder(t *testing.T) {
+	configuration := &Configuration{
+		Members: map[string]string{"1": "127.0.0.0:8080", "2": "127.0.0.1:8080"},
+		IsVoter: map[string]bool{"1": true, "2": false},
+	}
+
+	transport := &transport{}
+	encodedConfiguration, err := transport.EncodeConfiguration(configuration)
+	require.NoError(t, err)
+
+	decodedConfiguration, err := transport.DecodeConfiguration(encodedConfiguration)
+	require.NoError(t, err)
+
+	require.Equal(t, configuration, decodedConfiguration)
+}

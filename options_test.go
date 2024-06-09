1a979a44dc4366500a2f1b5418c77b92ded7e616
James <jmsadair@gmail.com>
2024-06-09 09:40:59 -0400

    Removed election and lease restrictions

--- options_test.go
+++ options_test.go
@@ -2,55 +2,9 @@ package raft
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/require"
 )
-
-// TestWithElectionTimeout checks that the election timeout option only accepts values within the
-// defined range.
-func TestWithElectionTimeout(t *testing.T) {
-	options := &options{}
-
-	// Test minimum bound
-	require.Error(t, WithElectionTimeout(minElectionTimeout-time.Millisecond)(options))
-
-	// Test maximum bound
-	require.Error(t, WithElectionTimeout(maxElectionTimeout+time.Millisecond)(options))
-
-	// Test valid input
-	require.NoError(t, WithElectionTimeout(500*time.Millisecond)(options))
-}
-
-// TestWithHeartbeatInterval checks that the heartbeat interval option only accepts values within the
-// defined range.
-func TestWithHeartbeatInterval(t *testing.T) {
-	options := &options{}
-
-	// Test minimum bound
-	require.Error(t, WithHeartbeatInterval(minHeartbeat-time.Millisecond)(options))
-
-	// Test maximum bound
-	require.Error(t, WithHeartbeatInterval(maxHeartbeat+time.Millisecond)(options))
-
-	// Test valid input
-	require.NoError(t, WithHeartbeatInterval(250*time.Millisecond)(options))
-}
-
-// TestWithLeaseDuration checks that the lease duration option only accepts values within the
-// defined range.
-func TestWithLeaseDuration(t *testing.T) {
-	options := &options{}
-
-	// Test minimum bound
-	require.Error(t, WithLeaseDuration(minLeaseDuration-time.Millisecond)(options))
-
-	// Test maximum bound
-	require.Error(t, WithLeaseDuration(maxLeaseDuration+time.Millisecond)(options))
-
-	// Test valid input
-	require.NoError(t, WithLeaseDuration(500*time.Millisecond)(options))
-}
 
 // TestWithLog checks that the log option only accepts non-nil logs.
 func TestWithLog(t *testing.T) {
